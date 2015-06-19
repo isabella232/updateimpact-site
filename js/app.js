fded9408c77e2257e1c660efcba2f3b2ff05ce49
adamw <adam@warski.org>
2015-06-19 12:28:23 +0200

    Voting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 (function() {
-  $('button.counter').on('click', function(){
-    // send info about click to server, and run next line
-    $(this).blur().addClass('clear').text('Thank you!');
-  })
+  $('button.counter').on('click', function(ev){
+  	var featureName = $(ev.target).closest('button').attr('data-id');
+  	var self = this;
+  	$.ajax({
+    	url: "https://app.updateimpact.com/rest/features/"+featureName,
+    	jsonp: "callback",
+	    dataType: "jsonp",
+	    success: function( response ) {
+    	    voted($(self));
+    	    $.cookie('feature-'+featureName, 'voted', { expires: 7 });
+    	}
+	});
+  });
+
+  function voted(el) {
+  	el.blur().addClass('clear').text('Thank you!');
+  }  
+
+  if ($.cookie('feature-browse')) voted($("#browse button.counter"));
+  if ($.cookie('feature-analyze')) voted($("#analyze button.counter"));
+  if ($.cookie('feature-define')) voted($("#define button.counter"));
+  if ($.cookie('feature-notify')) voted($("#notify button.counter"));
+  if ($.cookie('feature-compare')) voted($("#compare button.counter"));
 })();
